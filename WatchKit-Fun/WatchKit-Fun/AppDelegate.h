814aff51c065e08bd205790ca7ddecbc5627dfd0
Matthias Vermeulen <Matthias@MacBook-Pro-van-Matthias-2.local>
2014-11-24 15:47:32 +0100

    first commit

---
+++ WatchKit-Fun/WatchKit-Fun/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WatchKit-Fun
+//
+//  Created by Matthias Vermeulen on 24/11/14.
+//  Copyright (c) 2014 Noizy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
